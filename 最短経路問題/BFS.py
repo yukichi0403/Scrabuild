756c9d9a4a12dec2e7552de861d366449d23939a
yukichi0403 <bamboovalleyyuki@gmail.com>
2023-07-15 13:19:19 +0900

    fist commit

---
+++ 最短経路問題/BFS.py
@@ -0,0 +1,31 @@
+#BFS
+from collections import deque
+
+n, m, s, t = map(int, input().split())
+
+graph = [[] for _ in range(n)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a - 1].append(b - 1)
+    graph[b - 1].append(a - 1)
+
+for i in range(n):
+    graph[i].sort()
+
+q = deque()
+q.append(s - 1)
+#sからの距離を保持する配列を定義して-1で初期化
+dist = [-1] * n
+dist[s - 1] = 0
+
+while q:
+    #popleft()に変えるとDFSになる
+    now = q.pop()
+    for nxt in graph[now]:
+        #未探索の地点の場合のみ探索を続ける
+        if dist[nxt] == -1:
+            dist[nxt] = dist[now] + 1
+            q.append(nxt)
+
+#今回はtまでの距離を出力
+print(dist[t - 1])
