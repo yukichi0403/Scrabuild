fb5d260ea1096db0ae4a97bd260d653ab37c11b7
yukichi0403 <bamboovalleyyuki@gmail.com>
2023-07-16 23:14:12 +0900

    条件を＞に修正

--- 探索/条件付き二分探索（is_ok）.py
+++ 探索/条件付き二分探索（is_ok）.py
@@ -4,13 +4,13 @@ def is_ok(arg):
     return True,False
 
 def binary_search(left,right):
-    while right - left >= 1:
+    while right - left > 1:
         mid = (right + left) // 2
 
         if is_ok(mid):
             right = mid
         else:
             left = mid
-            
+
     #対象の値が単調増加する場合最終的にはright（大きい方）が条件を満たす最小値となる
     return right
