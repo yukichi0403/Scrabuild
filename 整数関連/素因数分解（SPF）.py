e1b594b05aa83352f1c53782107031e723766abe
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-07-22 22:05:01 +0900

    Create 素因数分解（SPF）.py

---
+++ 整数関連/素因数分解（SPF）.py
@@ -0,0 +1,19 @@
+def spf(n):
+  spf = [_ for _ in range(n+1)]; i = 2
+  while i*i <= n:
+    if spf[i] == i:
+      j = 2*i
+      while j <=n:
+        if spf[j] == j:
+          spf[j] = i
+          j += i
+    i += 1
+  return spf
+
+
+def PrimeFactorization(n):
+  spf_table = spf(n)
+  while n > 1:
+    print(spf_table[n])
+    n //= spf_table[n]
+
