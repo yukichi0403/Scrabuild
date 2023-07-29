2084fb78e886bd2af893f7e02b50dbd98699a83d
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-07-30 01:07:50 +0900

    Update 階乗.py

--- 整数関連/階乗.py
+++ 整数関連/階乗.py
@@ -3,22 +3,22 @@
 #MODなしfactorial、実行時間PyPyにて4000ms以上
 import math
 fact = [1]
-for n in range(1,10001):
+for n in range(1, 10001):
     fact.append(math.factorial(n))
 
 # Modなし 150ms程度
 fact = [1]
-for n in range(1,10001):
-    fact.append(fact[-1]*n)
+for n in range(1, 10001):
+    fact.append(fact[-1] * n)
 
 #MODありfactorial、実行時間PyPyにて4000ms以上
-MOD = 10**9+7
+MOD = 10 ** 9 + 7
 fact = [1]
-for n in range(1,10001):
-    fact.append(math.factorial(n)%MOD)
+for n in range(1, 10001):
+    fact.append(math.factorial(n) % MOD)
 
 #MODあり　100msを切る
-MOD = 10**9+7
+MOD = 10 ** 9 + 7
 fact = [1]
-for n in range(1,10001):
-    fact.append(fact[-1]*n%MOD)
+for n in range(1, 10001):
+    fact.append(fact[-1] * n % MOD)
