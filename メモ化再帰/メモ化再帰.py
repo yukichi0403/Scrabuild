eb8178aa6bce6b64f5dabb53c64235fd6848d090
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-08-04 00:08:57 +0900

    Update メモ化再帰.py

--- メモ化再帰/メモ化再帰.py
+++ メモ化再帰/メモ化再帰.py
@@ -1,11 +1,10 @@
 #maxsizeには、直近の何回分を記憶しておくかを指定する　Noneの場合は、制限なく記憶
-@functools.lru_cache(maxsize=None)
+from functools import lru_cache
 
+@lru_cache(maxsize=1000)
 #例としてフィボナッチ数列の場合
-def fibo(n):
-    if n <= 0:
-        return 0
-    elif n == 1:
-        return 1
+def fib_memoized(n):
+    if n < 2:
+        return n
     else:
-        return fibo(n-1) + fibo(n-2)
+        return fib(n - 1) + fib(n - 2)
