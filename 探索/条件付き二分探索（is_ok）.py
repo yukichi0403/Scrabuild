5cd6181c7e334b7a1d73580cd53489ba597cbee0
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-08-04 14:01:10 +0900

    Update 条件付き二分探索（is_ok）.py

--- 探索/条件付き二分探索（is_ok）.py
+++ 探索/条件付き二分探索（is_ok）.py
@@ -3,14 +3,16 @@ def is_ok(arg):
     #条件を満たすか問題ごとに定義
     return True,False
 
-def binary_search(left,right):
-    while right - left > 1:
-        mid = (right + left) // 2
-
+#修正
+def meguru_bisect(ng, ok):
+    while (abs(ok - ng) > 1):
+        mid = (ok + ng) // 2
         if is_ok(mid):
-            right = mid
+            ok = mid
         else:
-            left = mid
+            ng = mid
+            
+    #対象の値が単調増加する場合最終的にはokが条件を満たす最小値となる
+    return ok
 
-    #対象の値が単調増加する場合最終的にはright（大きい方）が条件を満たす最小値となる
-    return right
+    
