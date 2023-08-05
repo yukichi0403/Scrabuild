77a4e70d37d27d8f0984cbfda5037e02b4286de0
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-08-05 17:21:23 +0900

    Update 転倒数.py

--- ソート/転倒数.py
+++ ソート/転倒数.py
@@ -2,18 +2,23 @@ N = int(input())
 *A, = map(int, input().split())
 
 # BIT
-data = [0]*(N+1)
+# 0indexは使わないのでN+1
+BIT = [0] * (N+1)
 def add(k, x):
     while k <= N:
-      data[k] += x
+      BIT[k] += x
+    　　　#ビット演算にあたりkが０以下の場合は無限ループの陥るので注意
       k += k & -k
+        
 def get(k):
-    s = 0
+    ans = 0
     while k:
-        s += data[k]
+        ans += BIT[k]
+        #ビット演算にあたりkが０以下の場合は無限ループの陥るので注意
         k -= k & -k
-    return s
+    return ans
 
+#答えを求める
 ans = 0
 for i, a in enumerate(A):
     # 自分より小さい要素がいくつ存在するかを計算
