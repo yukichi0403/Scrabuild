25a4ca0ca9b88fcba3e23318a51fa63ae850b125
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-08-05 13:32:51 +0900

    Create ダイクストラver2.py

---
+++ 最短経路問題/ダイクストラver2.py
@@ -0,0 +1,62 @@
+
+import collections
+import heapq
+
+
+class Dijkstra():
+    def __init__(self):
+        self.e = collections.defaultdict(list)
+
+    def add(self, u, v, d, directed=False):
+        """
+        #0-indexedでなくてもよいことに注意
+        #u = from, v = to, d = cost
+        #directed = Trueなら、有向グラフである
+        """
+        if directed is False:
+            self.e[u].append([v, d])
+            self.e[v].append([u, d])
+        else:
+            self.e[u].append([v, d])
+
+    def delete(self, u, v):
+        self.e[u] = [_ for _ in self.e[u] if _[0] != v]
+        self.e[v] = [_ for _ in self.e[v] if _[0] != u]
+
+    def Dijkstra_search(self, s):
+        """
+        #0-indexedでなくてもよいことに注意
+        #:param s: 始点
+        #:return: 始点から各点までの最短経路と最短経路を求めるのに必要なprev
+        """
+        d = collections.defaultdict(lambda: float('inf'))
+        prev = collections.defaultdict(lambda: None)
+        d[s] = 0
+        q = []
+        heapq.heappush(q, (0, s))
+        v = collections.defaultdict(bool)
+        while len(q):
+            k, u = heapq.heappop(q)
+            if v[u]:
+                continue
+            v[u] = True
+
+            for uv, ud in self.e[u]:
+                if v[uv]:
+                    continue
+                vd = k + ud
+                if d[uv] > vd:
+                    d[uv] = vd
+                    prev[uv] = u
+                    heapq.heappush(q, (vd, uv))
+
+        return d, prev
+
+    def getDijkstraShortestPath(self, start, goal):
+        _, prev = self.Dijkstra_search(start)
+        shortestPath = []
+        node = goal
+        while node is not None:
+            shortestPath.append(node)
+            node = prev[node]
+        return shortestPath[::-1]
