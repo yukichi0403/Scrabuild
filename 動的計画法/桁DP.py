bc1c3b04f277fdd2a88d767b080613d5e6fccef6
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-08-08 11:57:52 +0900

    Update 桁DP.py

--- 動的計画法/桁DP.py
+++ 動的計画法/桁DP.py
@@ -7,22 +7,25 @@ b = int(input())
 def count(x):
     a = str(x)
     n = len(a)
-    #配列は末から
+    #内包表記上、配列は末から書く
     dp=[[[[[0] * 8 for _ in range(3)] for _ in range(2)] for _ in range(2)] for _ in range(n+1)]
     dp[0][0][0][0][0] = 1
 
     #条件に合わせてDP
     for i, less, has3, mod3, mod8 in product(range(n), (0,1), (0,1), range(3), range(8)):
+        #lessフラグがない場合は９まで、ある場合は指定された数のi桁目までの数字を上限にループ
         max_d = 9 if less else int(a[i])
         for d in range(max_d+1):
             less_ = less or d < max_d
             has3_ = has3 or d == 3
             mod3_ = (mod3 + d) % 3
+            #8の倍数は下2桁が8で割り切れるかどうかで見分けられるため、mod8の値を10で乗じてd足し上げることで2桁の数字にして判定。
             mod8_ = (mod8*10 + d) % 8
             dp[i + 1][less_][has3_][mod3_][mod8_] += dp[i][less][has3][mod3][mod8]
     
     #合致するものを合算
     ret = 0
+    #lessフラグは両方足し上げる、mod８フラグは1~7まで（余りあり）を足し上げる
     for less, mod8 in product((0,1), range(1,8)):
             ret += dp[n][less][1][0][mod8]
             ret += dp[n][less][1][1][mod8]
@@ -30,5 +33,6 @@ def count(x):
             ret += dp[n][less][0][0][mod8]
     return ret
 
+#a以上b以下の条件に当てはまる数を出力（以下なので、b-1)
 print(count(a) - count(b-1))
 
