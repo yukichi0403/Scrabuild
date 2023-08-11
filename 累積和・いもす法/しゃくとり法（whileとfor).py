fb8a36c6f9ccea7e14b1d1d88e38cb06b1d80e91
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-08-11 13:02:59 +0900

    Create しゃくとり法（whileとfor).py

---
+++ 累積和・いもす法/しゃくとり法（whileとfor).py
@@ -0,0 +1,15 @@
+right = 0
+for left in range(n):
+    while right < n and (right を 1 個進めたときに条件を満たす):
+        # 実際に right を 1 進める
+        # ex: sum += a[right]
+        right += 1
+
+    # break した状態で right は条件を満たす最大なので、何かする
+    # ex: res += (right - left)
+
+    # left をインクリメントする準備
+    # ex: if right == left:
+    #     right += 1
+    # else:
+    #     sum -= a[left]
