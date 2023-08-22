86755d9d87b0fe370a9fd1d790746b9561fdb77f
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-08-22 20:05:29 +0900

    Update BFS.py

--- 最短経路問題/BFS.py
+++ 最短経路問題/BFS.py
@@ -1,23 +1,23 @@
-from collections import deque
-
 #BFS
 N = int(input())
 S = [list(map(int, input().split())) for _ in range(N - 1)]
+
 # 無向グラフの典型入力
-from collections import defaultdict
+from collections import deque,defaultdict
 adj = defaultdict(list)
 for a, b in S:
     adj[a-1].append(b-1)
     adj[b-1].append(a-1)
 
-def dfs(start):
+def bfs(start):
     todo = deque()
     todo.append((0, start))   # 初期探索場所をpush
     seen = [False] * N
 
     while todo:
-        dist, pos = todo.pop()   # popleftにするとDFS
-        
+        dist, pos = todo.popleft()   # popにするとDFS
+
+        #訪問済みの場合はスルー
         if seen[pos]:
             continue
         seen[pos] = True
