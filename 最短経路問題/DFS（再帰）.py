1e36603f4f8ebbb171e5e36b3b16ffab8bb60349
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-08-23 22:15:43 +0900

    Update DFS（再帰）.py

--- 最短経路問題/DFS（再帰）.py
+++ 最短経路問題/DFS（再帰）.py
@@ -1,30 +1,31 @@
-N = int(input())
-S = [list(map(int, input().split())) for _ in range(N - 1)]
-
-# 無向グラフの典型入力
-from collections import defaultdict
-adj = defaultdict(list)
-for a, b in S:
-    adj[a-1].append(b-1)
-    adj[b-1].append(a-1)
-
 import sys
 sys.setrecursionlimit(10 ** 9)
 
-def dfs(pos, visited):   
+def dfs(G, pos, visited):   
     visited[pos] = True
     # いきがけ順の処理（帰りがけの処理とどっちがいいかは問題次第）
     """問題ごとに追記（path.append(pos)など）"""
     
     # 次の位置を探索する
-    for next_ in adj[pos]:
-        if visited[next_]:
+    for next_pos in G[pos]:
+        if visited[next_pos]:
             continue
-        dfs(next_, visited)
+        dfs(G, next_pos, visited)
 
     # 帰りがけ順の処理
     """問題ごとに追記（path.append(pos)など）"""
 
 
-dfs(0, [False] * N)
+# 頂点数と辺数
+N, M = map(int, input().split())
+
+# グラフ入力受取
+G = [[] for _ in range(N)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    G[a - 1].append(b - 1)
+    G[b - 1].append(a - 1)
+
+
+dfs(G, 0, [False] * N)
 
