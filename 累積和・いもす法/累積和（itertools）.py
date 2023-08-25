9b65170e66e102dea861f2e26cc9cdd3d7d6587f
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-08-25 22:48:22 +0900

    Update 累積和（itertools）.py

--- 累積和・いもす法/累積和（itertools）.py
+++ 累積和・いもす法/累積和（itertools）.py
@@ -1,10 +1,10 @@
 from itertools import accumulate
 
 L = list(range(1,7)) #[1,2,3,4,5,6]
-AC = list(accumulate(L)) #[1,3,6,10,15,21]
+AC = list(itertools.accumulate(L))+[0] #[1,3,6,10,15,21,0]
 
-#インデックスずれが起きないように AND lが０でもエラーにならないように先頭に0を追加
-AC.insert(0,0)
 
-l,r = 3,5
-print(AC[r]-AC[l-1])# 12 AC[r]からAC[l-1]を引くと範囲となる
+l,r=0,4
+print(AC[r]-AC[l-1])# 15 (AC[4]：15 - AC[-1]:0 = 15)
+#末尾に0をつけておけば１つ目の値単体の累積和もそのまま求められる
+
