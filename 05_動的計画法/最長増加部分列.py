ad2da7663d7d91b9b4e624ebd10827b5623c2fa9
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-09-08 14:43:41 +0900

    Update 最長増加部分列.py

--- 05_動的計画法/最長増加部分列.py
+++ 05_動的計画法/最長増加部分列.py
@@ -3,11 +3,18 @@ from bisect import bisect
 # N: 数列の長さ
 # A[i]: a_i の値
 def lis(N, A):
-    INF = 10**10
+    INF = float('inf')
 
     dp = [INF]*(N+1)
     dp[0] = -1
-    for a in A:
-        idx = bisect(dp, a-1)
-        dp[idx] = min(a, dp[idx])
+    # A[i]ごとにLISの長さを出したい場合
+    # dp2 = [INF] * N
+    for i in range(N):
+        #dp上のインデックス
+        idx = bisect(dp, A[i] - 1)
+        dp[idx] = min(A[i], dp[idx])
+        # A[i]ごとにその時点でのLISの長さを求めたい場合
+        # dp2[i] = idx
     return max(i for i in range(N+1) if dp[i] < INF)
+    
+    
