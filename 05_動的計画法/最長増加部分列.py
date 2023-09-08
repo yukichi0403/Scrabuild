51ea580646d2c1b10a1e9fbd6613485f30c03155
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-09-08 15:04:26 +0900

    Update 最長増加部分列.py

--- 05_動的計画法/最長増加部分列.py
+++ 05_動的計画法/最長増加部分列.py
@@ -7,14 +7,17 @@ def lis(N, A):
 
     dp = [INF]*(N+1)
     dp[0] = -1
-    # A[i]ごとに連続なLISの長さを出したい場合
+    # A[i]ごとに非連続なLISの長さを出したい場合
     # dp2 = [INF] * N
     for i in range(N):
-        #idx = dp上のインデックス = その時点（A[i]まで）で非連続なLISの長さ
+        #idx = dp上のインデックス = A全体におけるその時点（A[i]まで）で非連続なLISの長さ
         idx = bisect(dp, A[i] - 1)
         dp[idx] = min(A[i], dp[idx])
-        # その時点（A[i]まで）において連続なLISの長さ
+        # A[i]ごとの非連続なLISの長さ
         # dp2[i] = idx
     return max(i for i in range(N+1) if dp[i] < INF)
-    
+
+# 例: A = [3,1,4,1,5,9,2,6]
+# 全体LIS:         [1, 2, 5, 6, inf, inf, inf, inf]
+# その時点までのLIS: [1, 1, 2, 1, 3, 4, 2, 4]
     
