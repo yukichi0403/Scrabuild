29bfae0b8d20d5108f3b54431c8d1c23173962c9
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-09-10 21:08:16 +0900

    Update 10進数⇆n進数への変換.py

--- 03_整数関連/10進数⇆n進数への変換.py
+++ 03_整数関連/10進数⇆n進数への変換.py
@@ -5,8 +5,8 @@ def Base_10_to_n(X, n):
     return str(X % n)
 
 #n進数を10進数の整数Xに変換する関数
-def Base_n_to_10(X,　n):
+def Base_n_to_10(X, n):
     out = 0
     for i in range(1,len(str(X))+1):
-        out += int(X[-i])　*　(n　**　(i　-　1))
-    return out　#int out
+        out += int(X[-i]) * (n ** (i - 1))
+    return out #int out
