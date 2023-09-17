01ca7757a07a948f462dcd2a70bc80bc267bd5d7
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-09-17 23:53:33 +0900

    Update ランレングス圧縮.py

--- 01_基本のアルゴリズム/ランレングス圧縮.py
+++ 01_基本のアルゴリズム/ランレングス圧縮.py
@@ -9,19 +9,13 @@ def runLengthEncode(S: str) -> "List[tuple(str, int)]":
         res.append((k, int(len(list(v)))))
     return res
 
-# RUN LENGTH DECODING list(tuple()) -> str
-# example) [('a', 2), ('b', 4), ('a', 2), ('c', 1), ('a', 1)] -> "aabbbbaaca"
-def runLengthDecode(L: "list[tuple]") -> str:
-    res = ""
-    for c, n in L:
-        res += c * int(n)
-    return res
 
-# RUN LENGTH ENCODING str -> str
-# example) "aabbbbaaca" -> "a2b4a2c1a1" 
-def runLengthEncodeToString(S: str) -> str:
-    grouped = groupby(S)
-    res = ""
-    for k, v in grouped:
-        res += k + str(len(list(v)))
-    return res
+# itertools groupby
+'''
+import itertools
+l = [0, 0, 0, 1, 1, 2, 0, 0]
+print([(k, list(g)) for k, g in itertools.groupby(l)])
+'''
+# [(0, [0, 0, 0]), (1, [1, 1]), (2, [2]), (0, [0, 0])]
+
+
