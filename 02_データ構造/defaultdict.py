4c2ec35440436a8cca03678d8fa8dc95eedb4e53
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-09-25 23:46:42 +0900

    Update defaultdict.py

--- 02_データ構造/defaultdict.py
+++ 02_データ構造/defaultdict.py
@@ -1,10 +1,13 @@
 #普通の辞書と違って存在しないキーに参照してもエラーにならない
+#dictなのでlenはできないので注意
 from collections import defaultdict
 
 A=[int(i) for i in input().split()]
 dd=defaultdict(int)
 for a in A:
     dd[a]+=1
+
+
 
 #キーでソートする場合
 #降順でのソート
