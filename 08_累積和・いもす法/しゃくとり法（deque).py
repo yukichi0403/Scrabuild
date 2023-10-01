ecd5ee36e885bf6c193b5b0aa43e732f73c5aea4
yukichi0403 <139464700+yukichi0403@users.noreply.github.com>
2023-10-01 21:21:27 +0900

    Update しゃくとり法（deque).py

--- 08_累積和・いもす法/しゃくとり法（deque).py
+++ 08_累積和・いもす法/しゃくとり法（deque).py
@@ -7,6 +7,9 @@ a = [int(input()) for i in range(n)]
 q = deque()
 ans = 0
 tmp = 0
+
+# 部分文字列の個数を数える場合
+# for r,c in enumerate(s):
 for c in a:
     q.append(c)  ## dequeの右端に要素を一つ追加する。
     (追加した要素に応じて何らかの処理を行う)
@@ -16,6 +19,9 @@ for c in a:
         rm = q.popleft() ## 条件を満たさないのでdequeの左端から要素を取り除く
         (取り除いた要素に応じて何らかの処理を行う)
         # vtmp -= rm
+
+        # 部分文字列の個数を数える場合
+        # ans += N - r
 
     (dequeに入っている連続部分列は条件を満たしている。特に右端の要素から左に延ばせる最大の長さになっている。)
     # ans = max(ans, len(q))
